2cd53e0a13304880aac623f89ed79bf6ba72e732
Gregory <firzamdigidix@gmail.com>
2021-07-21 11:43:59 +0200

    Challenge 2

--- main.py
+++ main.py
@@ -3,10 +3,18 @@
 #    - Kevin PEETERS
 #    - Gregory MOU KUI
 import requests
+import json
+
+def formatJson(jsonResponse):
+    json = {}
+    for indexCapteur, capteur in enumerate(jsonResponse):
+        capteurData = {}
+        for indexData, data in enumerate(capteur):
+            capteurData[indexData] = data
+        json[indexCapteur] = capteurData
+    return json
 
 if __name__ == "__main__":
-    print('hello kevin peeters')
     response = requests.get("http://app.objco.com:8099/?account=BJ776QUVG0&limit=5")
-    #print(response.json())
-    for capteur in response.json():
-        print(capteur)
+    json = formatJson(response.json())
+    print(json)
