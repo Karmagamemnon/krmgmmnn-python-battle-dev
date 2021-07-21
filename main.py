eb5e68b6a70932e81bd25f91c82f2ed29af0e3c2
Gregory <firzamdigidix@gmail.com>
2021-07-21 11:16:50 +0200

    Challenge 1

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # Groups compose of:
 #    - Kevin PEETERS
 #    - Gregory MOU KUI
+import requests
 
 if __name__ == "__main__":
     print('hello kevin peeters')
+    response = requests.get("http://app.objco.com:8099/?account=BJ776QUVG0&limit=5")
+    print(response.json())
