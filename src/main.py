bbdefcd3942389d58e9e51a2c73c023ed003047d
Gregory <firzamdigidix@gmail.com>
2021-07-22 16:57:05 +0200

    Add get route for last data from all sensors

--- src/main.py
+++ src/main.py
@@ -28,9 +28,13 @@ def index():
 
 @app.route("/data")
 def getData():
-    query = "SELECT * FROM (SELECT * FROM `data` ORDER BY `timestamp` DESC) AS orderBy GROUP BY `id_sensor`"
-    print(executeSelectQuery(query))
-    return str(executeSelectQuery(query))
+    query = "SELECT `data1`.`timestamp`, `data1`.`temperature`, `data1`.`humidity`, `data1`.`rssi`, `data1`.`battery_voltage_status`, `data1`.`id_sensor` FROM `data` as data1 JOIN (SELECT * FROM `data` as data2 GROUP BY `data2`.`timestamp` ORDER BY `data2`.`timestamp` DESC) as data3 ON `data3`.`id` = `data1`.`id` GROUP BY `data1`.`id_sensor`"
+    result = executeSelectQuery(query)
+    listData = []
+    for row in result:
+        data = Data(None, row[0],row[1],row[2],row[3],row[4],row[5])
+        listData.append(data)
+    return jsonify(dataList=[data.serialize() for data in listData])
 
 
 def getLastSamples():
