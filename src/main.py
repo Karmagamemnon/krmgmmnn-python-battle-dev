39b75730d5f9cdd02d496fa3e3dd4ebc566f663c
Gregory <firzamdigidix@gmail.com>
2021-07-22 09:54:29 +0200

    Add threading to run update every 5 minutes

--- src/main.py
+++ src/main.py
@@ -2,10 +2,20 @@
 # Group composed of :
 #    - Kevin PEETERS
 #    - Gregory MOU KUI
-import requests
 import db
+import threading
+import time
+import requests
 from models.sample import Sample
 
+def getLastSamples():
+    response = requests.get(
+        "http://app.objco.com:8099/?account=BJ776QUVG0&limit=5")
+    samples = jsonToSamples(response.json())
+    for sample in samples:
+        db.saveSample(sample)
+        sample.decryptRawdata()
+    time.sleep(300)
 
 def jsonToSamples(jsonResponse):
     samples = []
@@ -16,9 +26,4 @@ def jsonToSamples(jsonResponse):
 
 
 if __name__ == "__main__":
-    response = requests.get(
-        "http://app.objco.com:8099/?account=BJ776QUVG0&limit=5")
-    samples = jsonToSamples(response.json())
-    for sample in samples:
-        db.saveSample(sample)
-        sample.decryptRawdata()
+    threading.Thread(target=getLastSamples()).start()
